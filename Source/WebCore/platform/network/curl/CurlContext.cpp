8959580e70a0b420c2402b1629b2611db15fbafe
Andrea Palmatè <info@papersoft.it>
2025-06-06 20:19:59 +0200

    Fixed curl was not loading any website

--- Source/WebCore/platform/network/curl/CurlContext.cpp
+++ Source/WebCore/platform/network/curl/CurlContext.cpp
@@ -291,14 +291,12 @@ CURLMcode CurlMultiHandle::getFdSet(fd_set& readFdSet, fd_set& writeFdSet, fd_se
 CURLMcode CurlMultiHandle::poll(const Vector<curl_waitfd>& extraFds, int timeoutMS)
 {
     int numFds = 0;
-// broken 2.34.6
     return curl_multi_poll(m_multiHandle, const_cast<curl_waitfd*>(extraFds.data()), extraFds.size(), timeoutMS, &numFds);
 }
 
 CURLMcode CurlMultiHandle::wakeUp()
 {
-// broken 2.34.6
-//    return curl_multi_wakeup(m_multiHandle);
+    return curl_multi_wakeup(m_multiHandle);
 }
 
 CURLMcode CurlMultiHandle::getTimeout(long &timeout)
